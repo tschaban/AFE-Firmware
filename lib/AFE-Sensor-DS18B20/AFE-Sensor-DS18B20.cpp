63c63865a2265b4aa1ba4994ca9c1fcd0ffeb899
tschaban <github@adrian.czabanowski.com>
2017-05-27 00:25:41 +0200

    Added DS18B20 library

---
+++ lib/AFE-Sensor-DS18B20/AFE-Sensor-DS18B20.cpp
@@ -0,0 +1,23 @@
+#include "AFE-Sensor-DS18B20.h"
+
+AFESensorDS18B20::AFESensorDS18B20(uint8_t gpio) {
+   _gpio = gpio;
+}
+
+float AFESensorDS18B20::get(uint8_t unit) {
+
+  OneWire wireProtocol(_gpio);
+  DallasTemperature sensor(&wireProtocol);
+  sensor.begin();
+
+  float temperature;
+  do {
+    sensor.requestTemperatures();
+    temperature = unit==UNIT_CELCIUS?sensor.getTempCByIndex(0):sensor.getTempFByIndex(0);
+  } while (temperature == 85.0 || temperature == (-127.0));
+  return temperature + _correction;
+}
+
+void AFESensorDS18B20::setCorrection(float correction) {
+  _correction = correction;
+}
---
+++ lib/AFE-Sensor-DS18B20/AFE-Sensor-DS18B20.h
@@ -0,0 +1,46 @@
+/*
+  DS18B20 sensor component
+  AFE Firmware for smart home devices build on ESP8266
+  More info: https://github.com/tschaban/AFE-Firmware
+  LICENCE: http://opensource.org/licenses/MIT
+*/
+
+#ifndef _AFE_Sensor_DS18B20_h
+#define _AFE_Sensor_DS18B20_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+#define UNIT_CELCIUS 0
+#define UNIT_FAHRENHEIT 1
+
+class AFESensorDS18B20
+{
+
+private:
+  uint8_t _gpio;
+	float _correction = 0;
+
+public:
+
+	/* Constructor: entry parameter is GPIO number where Sensor is connected to */
+	AFESensorDS18B20(uint8_t gpio);
+
+  /* Get current temp in Celsius (default) possible options:
+     - UNIT_CELCIUS
+		 - UNIT_FAHRENHEIT
+	*/
+	float get(uint8_t unit=0);
+
+  /* Set temperature correction value */
+	void setCorrection(float correction);
+
+};
+
+#endif
