4ce6a6bf16632cc4428e92fb7da42c337ed65c32
tschaban <github@adrian.czabanowski.com>
2017-05-26 21:57:45 +0200

    added code comments

--- lib/AFE-Relay/AFE-Relay.h
+++ lib/AFE-Relay/AFE-Relay.h
@@ -1,3 +1,10 @@
+/*
+  Relay component
+  AFE Firmware for smart home devices build on ESP8266
+  More info: https://github.com/tschaban/AFE-Firmware
+  LICENCE: http://opensource.org/licenses/MIT
+*/
+
 #ifndef _AFE_Relay_h
 #define _AFE_Relay_h
 
@@ -7,22 +14,32 @@
 	#include "WProgram.h"
 #endif
 
+/* Relay states */
 #define RELAY_ON 1
 #define RELAY_OFF 0
 
 class AFERelay
 {
 
-public:
-	AFERelay(uint8_t gpio);
-	uint8_t get();
-	void on();
-	void off();
-	void toggle();
-
 private:
 	uint8_t _gpio;
 
+public:
+
+	/* Constructor: entry parameter is GPIO number where Relay is connected to */
+	AFERelay(uint8_t gpio);
+
+	/* Returns 0 if relay is OFF, 1 if relay is ON */
+	uint8_t get();
+
+	/* Turns on relay */
+	void on();
+
+	/* Turns off relay */
+	void off();
+
+	/* Toggles relay state from ON to OFF or from OFF to ON */
+	void toggle();
 };
 
 #endif
