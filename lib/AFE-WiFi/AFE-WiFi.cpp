b67383e50863c07335a4f01fb696c54bf45720b9
tschaban <github@adrian.czabanowski.com>
2017-05-27 23:55:39 +0200

    initial WiFi library

---
+++ lib/AFE-WiFi/AFE-WiFi.cpp
@@ -0,0 +1,19 @@
+#include "AFE-Wifi.h"
+
+AFEWiFi::AFEWiFi() {}
+
+void AFEWiFi::connect(char* ssid, char* password) {
+    uint8_t connection_try = 0;
+    WiFi.hostname("TEST");
+    WiFi.begin(ssid,password);
+    Serial << endl << "INFO: WiFi connection status: " << WiFi.status();
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial << endl << "INFO: WiFi connection attempt: " << connection_try << " from " << _no_connection_attempts;
+      connection_try++;
+      delay(_duration_between_connection_attempts * 1000);
+      if (connection_try == _no_connection_attempts) {
+      _sleepMode = true;
+      break;
+   }
+ }
+}
---
+++ lib/AFE-WiFi/AFE-WiFi.h
@@ -0,0 +1,36 @@
+/*
+  WiFi component
+  AFE Firmware for smart home devices build on ESP8266
+  More info: https://github.com/tschaban/AFE-Firmware
+  LICENCE: http://opensource.org/licenses/MIT
+*/
+
+#ifndef _AFE_WiFi_h
+#define _AFE_WiFi_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <ESP8266WiFi.h>
+#include <Streaming.h>
+
+class AFEWiFi {
+
+private:
+   boolean _accessPoint = false;
+	 uint8_t _no_connection_attempts = 20;
+   uint8_t _duration_between_connection_attempts = 1;
+   uint8_t _duration_between_next_connection_attempts_series = 60;
+	 boolean _sleepMode = false;
+
+public:
+	AFEWiFi();
+
+	void connect(char* ssid, char* password);
+
+};
+
+#endif
