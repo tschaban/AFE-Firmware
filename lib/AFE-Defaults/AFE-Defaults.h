13051e9e8dbea70453b57af24fc2e70958f9e1b8
tschaban <github@adrian.czabanowski.com>
2017-07-14 23:08:34 +0200

    Class saving default values

---
+++ lib/AFE-Defaults/AFE-Defaults.h
@@ -0,0 +1,31 @@
+/*
+   Default values
+   AFE Firmware for smart home devices build on ESP8266
+   More info: https://github.com/tschaban/AFE-Firmware
+   LICENCE: http://opensource.org/licenses/MIT
+ */
+
+#ifndef _AFE_Defaults_h
+#define _AFE_Defaults_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#include <AFE-EEPROM.h>
+#include <Streaming.h>
+
+
+class AFEDefaults
+{
+private:
+
+public:
+AFEDefaults();
+void set();
+
+};
+#endif
