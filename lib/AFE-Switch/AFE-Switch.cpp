1677edfc29ad13902eecdd00b492d1edf6a5c8bc
tschaban <github@adrian.czabanowski.com>
2017-10-19 23:22:45 +0200

    added init method

--- lib/AFE-Switch/AFE-Switch.cpp
+++ lib/AFE-Switch/AFE-Switch.cpp
@@ -1,33 +1,34 @@
 #include "AFE-Switch.h"
 
+AFESwitch::AFESwitch(){};
 
-AFESwitch::AFESwitch(uint8_t switch_gpio,uint8_t switch_type) {
+AFESwitch::AFESwitch(uint8_t switch_gpio, uint8_t switch_type) {
+  begin(switch_gpio, switch_type);
+}
+
+void AFESwitch::begin(uint8_t switch_gpio, uint8_t switch_type) {
   gpio = switch_gpio;
   pinMode(gpio, INPUT_PULLUP);
   state = digitalRead(gpio);
   type = switch_type;
 }
 
-boolean AFESwitch::isON() {
-  return state?false:true;
-}
+boolean AFESwitch::isON() { return state ? false : true; }
 
-boolean AFESwitch::isOFF() {
-  return state?true:false;
-}
+boolean AFESwitch::isOFF() { return state ? true : false; }
 
 void AFESwitch::listener() {
-   boolean currentState = digitalRead(gpio);
-   if (type==SWITCH_TYPE_MONO) {
-     state = currentState;
-   } else {
-     if (!currentState) {
-        if (!pressed) {
-          pressed = true;
-          state = !state;
-        }
-     } else {
-       pressed = false;
-     }
-   }
+  boolean currentState = digitalRead(gpio);
+  if (type == SWITCH_TYPE_BI) {
+    state = currentState;
+  } else {
+    if (!currentState) {
+      if (!pressed) {
+        pressed = true;
+        state = !state;
+      }
+    } else {
+      pressed = false;
+    }
+  }
 }
--- lib/AFE-Switch/AFE-Switch.h
+++ lib/AFE-Switch/AFE-Switch.h
@@ -9,18 +9,16 @@
 #define _AFE_Switch_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+#include "arduino.h"
 #else
-	#include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 /* Types of switch */
 #define SWITCH_TYPE_MONO 0 // Mono stable switch
 #define SWITCH_TYPE_BI 1   // Bi stabe switch
 
-
-class AFESwitch
-{
+class AFESwitch {
 
 private:
   uint8_t gpio;
@@ -28,23 +26,23 @@ private:
   boolean state;
   boolean pressed = false;
 
-
 public:
-
   /* Constructor: entry parameter is GPIO number where Switch is connected to
      second parameter defiens if the switch is mono or bi
   */
-  AFESwitch(uint8_t switch_gpio, uint8_t switch_type=SWITCH_TYPE_MONO);
+  AFESwitch();
+  AFESwitch(uint8_t switch_gpio, uint8_t switch_type = SWITCH_TYPE_MONO);
+
+  void begin(uint8_t switch_gpio, uint8_t switch_type = SWITCH_TYPE_MONO);
 
   /* Returns TRUE if switch is ON */
   boolean isON();
 
-	/* Returns TRUE if switch is OFF */
+  /* Returns TRUE if switch is OFF */
   boolean isOFF();
 
-	/* It has to be added to the loop in order to listen for switch changes */
+  /* It has to be added to the loop in order to listen for switch changes */
   void listener();
-
 };
 
 #endif
