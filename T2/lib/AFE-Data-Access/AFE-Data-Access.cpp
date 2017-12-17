1ab5862d90239ebf2ee150540aa3191838b45ea9
Czabanowski <czabanoa@emea.roche.com>
2017-12-17 17:19:51 +0100

    added DHT sensor related methods

---
+++ T2/lib/AFE-Data-Access/AFE-Data-Access.cpp
@@ -0,0 +1,265 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#include "AFE-Data-Access.h"
+
+AFEDataAccess::AFEDataAccess() {}
+
+DEVICE AFEDataAccess::getDeviceConfiguration() {
+  DEVICE configuration;
+
+  Eeprom.read(9, 16).toCharArray(configuration.name,
+                                 sizeof(configuration.name));
+  configuration.isLED[0] = Eeprom.read(366);
+  configuration.isRelay[0] = Eeprom.read(396);
+  configuration.isSwitch[0] = Eeprom.read(382);
+  configuration.isSwitch[1] = Eeprom.read(389);
+  configuration.isDHT = Eeprom.read(369);
+  configuration.httpAPI = Eeprom.read(25);
+  configuration.mqttAPI = Eeprom.read(228);
+
+  return configuration;
+}
+
+FIRMWARE AFEDataAccess::getFirmwareConfiguration() {
+  FIRMWARE configuration;
+
+  Eeprom.read(0, 7).toCharArray(configuration.version,
+                                sizeof(configuration.version));
+  Eeprom.read(28, 120).toCharArray(configuration.upgradeURL,
+                                   sizeof(configuration.upgradeURL));
+  configuration.type = Eeprom.readUInt8(7);
+  configuration.autoUpgrade = Eeprom.readUInt8(27);
+  return configuration;
+}
+
+NETWORK AFEDataAccess::getNetworkConfiguration() {
+  NETWORK configuration;
+
+  Eeprom.read(148, 32).toCharArray(configuration.ssid,
+                                   sizeof(configuration.ssid));
+  Eeprom.read(180, 32).toCharArray(configuration.password,
+                                   sizeof(configuration.password));
+
+  configuration.isDHCP = Eeprom.read(212);
+  configuration.ip = Eeprom.readIP(213);
+  configuration.gateway = Eeprom.readIP(217);
+  configuration.subnet = Eeprom.readIP(221);
+  configuration.noConnectionAttempts = Eeprom.readUInt8(225);
+  configuration.waitTimeConnections = Eeprom.readUInt8(226);
+  configuration.waitTimeSeries = Eeprom.readUInt8(227);
+  return configuration;
+}
+
+MQTT AFEDataAccess::getMQTTConfiguration() {
+  MQTT configuration;
+
+  Eeprom.read(229, 32).toCharArray(configuration.host,
+                                   sizeof(configuration.host));
+  configuration.ip = Eeprom.readIP(261);
+  configuration.port = Eeprom.read(265, 5).toInt();
+  Eeprom.read(270, 32).toCharArray(configuration.user,
+                                   sizeof(configuration.user));
+  Eeprom.read(302, 32).toCharArray(configuration.password,
+                                   sizeof(configuration.password));
+  Eeprom.read(334, 32).toCharArray(configuration.topic,
+                                   sizeof(configuration.topic));
+
+  return configuration;
+}
+
+LED AFEDataAccess::getLEDConfiguration(uint8_t id) {
+  LED configuration;
+  uint8_t nextLED = 0;
+  configuration.gpio = Eeprom.readUInt8(367 + id * nextLED);
+  configuration.changeToOppositeValue = Eeprom.read(368 + id * nextLED);
+
+  return configuration;
+}
+
+RELAY AFEDataAccess::getRelayConfiguration(uint8_t id) {
+  RELAY configuration;
+  MQTT configurationMQTT;
+  uint8_t nextRelay = 40;
+
+  char mqttTopic[49];
+  configuration.gpio = Eeprom.readUInt8(397 + id * nextRelay);
+  configuration.timeToOff = Eeprom.read(399 + id * nextRelay, 5).toFloat();
+  configuration.statePowerOn = Eeprom.readUInt8(404 + id * nextRelay);
+
+  Eeprom.read(405 + id * nextRelay, 16)
+      .toCharArray(configuration.name, sizeof(configuration.name));
+
+  configuration.stateMQTTConnected = Eeprom.readUInt8(421 + id * nextRelay);
+
+  Eeprom.read(334, 32).toCharArray(configurationMQTT.topic,
+                                   sizeof(configurationMQTT.topic));
+
+  sprintf(configuration.mqttTopic, "%s%s/", configurationMQTT.topic,
+          configuration.name);
+
+  configuration.showStatusUsingLED = Eeprom.read(422 + id * nextRelay);
+
+  configuration.thermostat.temperatureTurnOn =
+      Eeprom.read(423 + id * nextRelay, 6).toFloat();
+  configuration.thermostat.temperatureTurnOff =
+      Eeprom.read(428 + id * nextRelay, 6).toFloat();
+  configuration.thermostat.temperatureTurnOnAbove =
+      Eeprom.read(433 + id * nextRelay);
+  configuration.thermostat.temperatureTurnOffAbove =
+      Eeprom.read(434 + id * nextRelay);
+  configuration.thermostat.enabled = isThermostatEnabled(id);
+
+  configuration.thermalProtection =
+      Eeprom.read(436 + id * nextRelay, 3).toInt();
+
+  return configuration;
+}
+
+SWITCH AFEDataAccess::getSwitchConfiguration(uint8_t id) {
+  SWITCH configuration;
+  uint8_t nextSwitch = 7;
+  configuration.gpio = Eeprom.readUInt8(383 + id * nextSwitch);
+  configuration.type = Eeprom.readUInt8(384 + id * nextSwitch);
+  configuration.sensitiveness = Eeprom.read(385 + id * nextSwitch, 3).toInt();
+  configuration.functionality = Eeprom.readUInt8(388 + id * nextSwitch);
+
+  return configuration;
+}
+
+DH AFEDataAccess::getDHTConfiguration() {
+  DH configuration;
+  configuration.gpio = Eeprom.readUInt8(370);
+  configuration.temperature.correction = Eeprom.read(371, 5).toFloat();
+  configuration.temperature.interval = Eeprom.read(376, 5).toInt();
+  configuration.temperature.unit = Eeprom.readUInt8(381);
+  configuration.humidity.correction = Eeprom.read(371, 5).toFloat();
+  configuration.humidity.interval = Eeprom.read(376, 5).toInt();
+  return configuration;
+}
+
+void AFEDataAccess::saveConfiguration(DEVICE configuration) {
+  Eeprom.write(9, 16, configuration.name);
+  Eeprom.write(396, configuration.isRelay[0]);
+  Eeprom.write(382, configuration.isSwitch[0]);
+  Eeprom.write(389, configuration.isSwitch[1]);
+  Eeprom.write(366, configuration.isLED[0]);
+  Eeprom.write(369, configuration.isDS18B20);
+  Eeprom.write(25, configuration.httpAPI);
+  Eeprom.write(228, configuration.mqttAPI);
+}
+
+void AFEDataAccess::saveConfiguration(FIRMWARE configuration) {
+  Eeprom.write(0, 7, configuration.version);
+  Eeprom.writeUInt8(7, configuration.type);
+  Eeprom.writeUInt8(27, configuration.autoUpgrade);
+  Eeprom.write(28, 120, configuration.upgradeURL);
+}
+
+void AFEDataAccess::saveConfiguration(NETWORK configuration) {
+  Eeprom.write(148, 32, configuration.ssid);
+  Eeprom.write(180, 32, configuration.password);
+  Eeprom.write(212, configuration.isDHCP);
+  Eeprom.writeIP(213, configuration.ip);
+  Eeprom.writeIP(217, configuration.gateway);
+  Eeprom.writeIP(221, configuration.subnet);
+  Eeprom.writeUInt8(225, configuration.noConnectionAttempts);
+  Eeprom.writeUInt8(226, configuration.waitTimeConnections);
+  Eeprom.writeUInt8(227, configuration.waitTimeSeries);
+}
+
+void AFEDataAccess::saveConfiguration(MQTT configuration) {
+  Eeprom.write(229, 32, configuration.host);
+  Eeprom.writeIP(261, configuration.ip);
+  Eeprom.write(265, 5, (long)configuration.port);
+  Eeprom.write(270, 32, configuration.user);
+  Eeprom.write(302, 32, configuration.password);
+  Eeprom.write(334, 32, configuration.topic);
+}
+
+void AFEDataAccess::saveConfiguration(uint8_t id, RELAY configuration) {
+  uint8_t nextRelay = 40;
+
+  Eeprom.writeUInt8(397 + id * nextRelay, configuration.gpio);
+  Eeprom.write(399 + id * nextRelay, 5, configuration.timeToOff);
+  Eeprom.writeUInt8(404 + id * nextRelay, configuration.statePowerOn);
+  Eeprom.write(405 + id * nextRelay, 16, configuration.name);
+  Eeprom.writeUInt8(421 + id * nextRelay, configuration.stateMQTTConnected);
+
+  Eeprom.write(422 + id * nextRelay, configuration.showStatusUsingLED);
+  Eeprom.write(423 + id * nextRelay, 6,
+               configuration.thermostat.temperatureTurnOn);
+  Eeprom.write(428 + id * nextRelay, 6,
+               configuration.thermostat.temperatureTurnOff);
+  Eeprom.write(433 + id * nextRelay,
+               configuration.thermostat.temperatureTurnOnAbove);
+  Eeprom.write(434 + id * nextRelay,
+               configuration.thermostat.temperatureTurnOffAbove);
+  saveThermostatState(id, configuration.thermostat.enabled);
+  Eeprom.write(436 + id * nextRelay,3,
+               configuration.thermalProtection);
+}
+
+void AFEDataAccess::saveConfiguration(uint8_t id, LED configuration) {
+  uint8_t nextLED = 2;
+  Eeprom.writeUInt8(367 + id * nextLED, configuration.gpio);
+  Eeprom.write(368 + id * nextLED, configuration.changeToOppositeValue);
+}
+
+void AFEDataAccess::saveConfiguration(uint8_t id, SWITCH configuration) {
+  uint8_t nextSwitch = 7;
+  Eeprom.writeUInt8(383 + id * nextSwitch, configuration.gpio);
+  Eeprom.writeUInt8(384 + id * nextSwitch, configuration.type);
+  Eeprom.write(385 + id * nextSwitch, 3, (long)configuration.sensitiveness);
+  Eeprom.writeUInt8(388 + id * nextSwitch, configuration.functionality);
+}
+
+void AFEDataAccess::saveConfiguration(DH configuration) {
+  Eeprom.writeUInt8(370, configuration.gpio);
+  Eeprom.write(371, 5, (float)configuration.temperature.correction);
+  Eeprom.write(376, 5, (long)configuration.temperature.interval);
+  Eeprom.writeUInt8(381, configuration.temperature.unit);
+  Eeprom.write(371, 5, (float)configuration.humidity.correction);
+  Eeprom.write(376, 5, (long)configuration.humidity.interval);
+}
+
+const char AFEDataAccess::getVersion() {
+  char version[7];
+  Eeprom.read(0, 7).toCharArray(version, sizeof(version));
+  return *version;
+}
+
+void AFEDataAccess::saveVersion(String version) { Eeprom.write(0, 7, version); }
+
+boolean AFEDataAccess::getRelayState(uint8_t id) {
+  uint8_t nextRelay = 39;
+  return Eeprom.read(398 + id * nextRelay);
+}
+
+void AFEDataAccess::saveRelayState(uint8_t id, boolean state) {
+  uint8_t nextRelay = 39;
+  Eeprom.write(398 + id * nextRelay, state);
+}
+
+uint8_t AFEDataAccess::getDeviceMode() { return Eeprom.readUInt8(26); }
+
+void AFEDataAccess::saveDeviceMode(uint8_t mode) {
+  Eeprom.writeUInt8(26, mode);
+}
+
+uint8_t AFEDataAccess::getLanguage() { return Eeprom.readUInt8(8); }
+
+void AFEDataAccess::saveLanguage(uint8_t language) {
+  Eeprom.writeUInt8(8, language);
+}
+
+boolean AFEDataAccess::isThermostatEnabled(uint8_t id) {
+  uint8_t nextRelay = 40;
+  return Eeprom.read(435 + id * nextRelay);
+}
+
+void AFEDataAccess::saveThermostatState(uint8_t id, boolean state) {
+  uint8_t nextRelay = 40;
+  Eeprom.write(435 + id * nextRelay, state);
+}
---
+++ T2/lib/AFE-Defaults/AFE-Defaults.cpp
@@ -0,0 +1,124 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#include "AFE-Defaults.h"
+
+AFEDefaults::AFEDefaults() {}
+
+const char *AFEDefaults::getFirmwareVersion() { return "1.0.1"; }
+uint8_t AFEDefaults::getFirmwareType() { return 1; }
+void AFEDefaults::set() {
+
+  AFEDataAccess *Data;
+
+  DEVICE deviceConfiguration;
+  FIRMWARE firmwareConfiguration;
+  NETWORK networkConfiguration;
+  MQTT MQTTConfiguration;
+  RELAY RelayConfiguration;
+  SWITCH SwitchConfiguration;
+  LED LEDConfiguration;
+  DH DHTConfiguration;
+
+  sprintf(firmwareConfiguration.version, getFirmwareVersion());
+  firmwareConfiguration.type = getFirmwareType();
+  firmwareConfiguration.autoUpgrade = 0;
+  sprintf(firmwareConfiguration.upgradeURL, "");
+
+  Data->saveConfiguration(firmwareConfiguration);
+
+  sprintf(deviceConfiguration.name, "AFE-Device");
+  deviceConfiguration.isLED[0] = true;
+  deviceConfiguration.isRelay[0] = true;
+  deviceConfiguration.isSwitch[0] = true;
+  deviceConfiguration.isSwitch[1] = false;
+  deviceConfiguration.isDHT = false;
+  deviceConfiguration.mqttAPI = false;
+  deviceConfiguration.httpAPI = true;
+
+  Data->saveConfiguration(deviceConfiguration);
+
+  sprintf(networkConfiguration.ssid, "");
+  sprintf(networkConfiguration.password, "");
+  networkConfiguration.isDHCP = true;
+  networkConfiguration.ip = IPAddress(0, 0, 0, 0);
+  networkConfiguration.gateway = IPAddress(0, 0, 0, 0);
+  networkConfiguration.subnet = IPAddress(255, 255, 255, 0);
+  networkConfiguration.noConnectionAttempts = 10;
+  networkConfiguration.waitTimeConnections = 1;
+  networkConfiguration.waitTimeSeries = 60;
+
+  Data->saveConfiguration(networkConfiguration);
+
+  sprintf(MQTTConfiguration.host, "");
+  MQTTConfiguration.ip = IPAddress(0, 0, 0, 0);
+  sprintf(MQTTConfiguration.user, "");
+  sprintf(MQTTConfiguration.password, "");
+  MQTTConfiguration.port = 1883;
+  // sprintf(MQTTConfiguration.topic, "/device/");
+  sprintf(MQTTConfiguration.topic, "/device/");
+
+  Data->saveConfiguration(MQTTConfiguration);
+
+  RelayConfiguration.gpio = 12;
+  RelayConfiguration.timeToOff = 0;
+  RelayConfiguration.statePowerOn = 3;
+  RelayConfiguration.stateMQTTConnected = 0;
+  sprintf(RelayConfiguration.name, "switch");
+
+  RelayConfiguration.thermostat.enabled = false;
+  RelayConfiguration.thermostat.temperatureTurnOn = 0;
+  RelayConfiguration.thermostat.temperatureTurnOnAbove = false;
+  RelayConfiguration.thermostat.temperatureTurnOff = 0;
+  RelayConfiguration.thermostat.temperatureTurnOffAbove = true;
+  RelayConfiguration.thermalProtection = 0;
+
+  Data->saveConfiguration(0, RelayConfiguration);
+
+  /* @TODO DOMOTICZ
+  RelayConfiguration.idx = 0;
+  RelayConfiguration.publishToDomoticz = false;
+  */
+
+  SwitchConfiguration.gpio = 0;
+  SwitchConfiguration.type = 0;
+  SwitchConfiguration.sensitiveness = 50;
+  SwitchConfiguration.functionality = 0;
+  Data->saveConfiguration(0, SwitchConfiguration);
+
+  SwitchConfiguration.gpio = 5;
+  SwitchConfiguration.type = 1;
+  SwitchConfiguration.functionality = 11;
+  Data->saveConfiguration(1, SwitchConfiguration);
+
+  LEDConfiguration.gpio = 13;
+  LEDConfiguration.changeToOppositeValue = false;
+  Data->saveConfiguration(0, LEDConfiguration);
+
+  DHTConfiguration.gpio = 14;
+  DHTConfiguration.temperature.correction = 0;
+  DHTConfiguration.temperature.interval = 60;
+  DHTConfiguration.temperature.unit = 0;
+  DHTConfiguration.humidity.correction = 0;
+  DHTConfiguration.humidity.interval = 60;
+
+  Data->saveConfiguration(DHTConfiguration);
+
+  /* @TODO DOMOTICZ
+  Serial << endl << "INFO: Setting defaults: domoticz";
+  sprintf(DomoticzConfiguration.host, "");
+  DomoticzConfiguration.ip = IPAddress(0, 0, 0, 0);
+  sprintf(DomoticzConfiguration.user, "");
+  sprintf(DomoticzConfiguration.password, "");
+  DomoticzConfiguration.port = 8080;
+
+Data->saveConfiguration(DomoticzConfiguration);
+  
+  */
+  Data->saveDeviceMode(2);
+  Data->saveRelayState(0, false);
+  Data->saveLanguage(1);
+}
+
+void AFEDefaults::eraseConfiguration() { Eeprom.erase(); }
