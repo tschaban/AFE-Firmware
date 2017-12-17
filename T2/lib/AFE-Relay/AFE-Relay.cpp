2f14026bc6b9bbdabc1441c1fe93b1588c97e8ba
Czabanowski <czabanoa@emea.roche.com>
2017-12-17 17:20:15 +0100

    Initials class definition added

---
+++ T2/lib/AFE-Relay/AFE-Relay.cpp
@@ -0,0 +1,106 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#include "AFE-Relay.h"
+
+AFERelay::AFERelay(){};
+
+AFERelay::AFERelay(uint8_t id) { begin(id); }
+
+void AFERelay::begin(uint8_t id) {
+  MQTT MQTTConfiguration;
+  MQTTConfiguration = Data.getMQTTConfiguration();
+  _id = id;
+  RelayConfiguration = Data.getRelayConfiguration(_id);
+  pinMode(RelayConfiguration.gpio, OUTPUT);
+  sprintf(mqttTopic, "%s%s/", MQTTConfiguration.topic, RelayConfiguration.name);
+  /* Initialzing Thermostat functionality for a relay */
+  Thermostat.begin(_id,RelayConfiguration.thermostat);
+  /* Initialzing thermal protection functionality for a relay */
+  ThermalProtection.begin(RelayConfiguration.thermalProtection);
+}
+
+const char *AFERelay::getMQTTTopic() { return RelayConfiguration.mqttTopic; }
+
+byte AFERelay::get() {
+  return digitalRead(RelayConfiguration.gpio) == HIGH ? RELAY_ON : RELAY_OFF;
+}
+
+/* Set relay to ON */
+void AFERelay::on() {
+  if (get() == RELAY_OFF) {
+    digitalWrite(RelayConfiguration.gpio, HIGH);
+    if (RelayConfiguration.timeToOff >
+        0) { // Start counter if relay should be automatically turned off
+      turnOffCounter = millis();
+    }
+  }
+  Data.saveRelayState(_id, RELAY_ON);
+}
+
+/* Set relay to OFF */
+void AFERelay::off() {
+  if (get() == RELAY_ON) {
+    digitalWrite(RelayConfiguration.gpio, LOW);
+  }
+  Data.saveRelayState(_id, RELAY_OFF);
+}
+
+/* Toggle relay */
+void AFERelay::toggle() {
+  if (digitalRead(RelayConfiguration.gpio) == LOW) {
+    on();
+  } else {
+    off();
+  }
+}
+
+void AFERelay::setRelayAfterRestoringPower() {
+  setRelayAfterRestore(RelayConfiguration.statePowerOn);
+}
+
+boolean AFERelay::setRelayAfterRestoringMQTTConnection() {
+  if (RelayConfiguration.stateMQTTConnected ==
+      5) { // request state from MQTT Broker
+    return false;
+  } else {
+    setRelayAfterRestore(RelayConfiguration.stateMQTTConnected);
+    return true;
+  }
+}
+
+void AFERelay::setRelayAfterRestore(uint8_t option) {
+
+  if (option == 1) {
+    off();
+  } else if (option == 2) {
+    on();
+  } else if (option == 3) {
+    Data.getRelayState(_id) == RELAY_ON ? on() : off();
+  } else if (option == 4) {
+    Data.getRelayState(_id) == RELAY_ON ? off() : on();
+  }
+}
+
+boolean AFERelay::autoTurnOff() {
+  if (RelayConfiguration.timeToOff > 0 && get() == RELAY_ON &&
+      millis() - turnOffCounter >= RelayConfiguration.timeToOff * 1000) {
+    off();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const char *AFERelay::getName() { return RelayConfiguration.name; }
+
+/*
+THERMOSTAT AFERelay::getConfiguration() {
+  return RelayConfiguration.thermostat;
+}
+
+boolean AFERelay::thermostatEnabled() {
+  return RelayConfiguration.thermostat.enabled;
+}
+*/
---
+++ T2/lib/AFE-Relay/AFE-Relay.h
@@ -0,0 +1,79 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_Relay_h
+#define _AFE_Relay_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <AFE-Data-Access.h>
+#include <AFE-Thermostat.h>
+#include <AFE-Thermal-Protection.h>
+
+//#include <Streaming.h>
+
+class AFERelay {
+
+private:
+  uint8_t _id;
+  AFEDataAccess Data; // @TODO nie jest konsekwentnie jak np. w switch
+  RELAY RelayConfiguration;
+  char mqttTopic[50];
+  unsigned long turnOffCounter = 0;
+
+  /* Method set relay state after power restore or connection to MQTT is
+   * established */
+  void setRelayAfterRestore(uint8_t option);
+
+public:
+
+  AFEThermostat Thermostat;
+  AFEThermalProtection ThermalProtection;
+
+  /* Constructors */
+  AFERelay();
+  AFERelay(uint8_t id);
+
+
+
+  /* Method: initiates relay */
+  void begin(uint8_t id);
+
+  /* Method returns MQTT topic for this relay */
+  const char *getMQTTTopic();
+
+  /* Method sets relay state after device is turned on / power is restored / or
+   * after device has been crash */
+  void setRelayAfterRestoringPower();
+
+  /* Method sets relay state after device is connected / reconnected to MQTT
+   * Broker. It returns TRUE if relay state has been set, false it relay state
+   * should be manged through MQTT Broker*/
+  boolean setRelayAfterRestoringMQTTConnection();
+
+  /* Method: Returns RELAY_OFF if relay is OFF, RELAY_ON if relay is ON */
+  byte get();
+
+  /* Turns on relay */
+  void on();
+
+  /* Turns off relay */
+  void off();
+
+  /* Toggles relay state from ON to OFF or from OFF to ON */
+  void toggle();
+
+  /* Methods returns relay name */
+  const char *getName();
+
+  /* Methods while added to main loop turns off relay automatically. Duration
+   * how long relay should be on must be configured */
+  boolean autoTurnOff();
+};
+
+#endif
