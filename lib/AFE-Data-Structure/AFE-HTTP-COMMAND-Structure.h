533404c10752701d81de1bdd26a022ae46bbd82c
Czabanowski <czabanoa@emea.roche.com>
2018-01-14 23:28:48 +0100

    HTTP Request structure

---
+++ lib/AFE-Data-Structure/AFE-HTTP-COMMAND-Structure.h
@@ -0,0 +1,22 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_HTTP_COMMAND_Structure_h
+#define _AFE_HTTP_COMMAND_Structure_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <ESP8266WiFi.h>
+
+struct HTTPCOMMAND {
+  char device[16];
+  char name[16];
+  char command[32];
+};
+
+#endif
