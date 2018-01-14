514a24bdfc17a88a59bcfb184d60bcfe870007cc
Czabanowski <czabanoa@emea.roche.com>
2018-01-14 23:27:20 +0100

    DHT Structure

---
+++ lib/AFE-Data-Structure/AFE-DHT-Sensor-Structure.h
@@ -0,0 +1,25 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_DHT_Structure_h
+#define _AFE_DHT_Structure_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <AFE-HUMIDITY-Structure.h>
+#include <AFE-TEMPERATURE-Structure.h>
+#include <ESP8266WiFi.h>
+
+struct DH {
+  uint8_t gpio;
+  uint8_t type;
+  TEMPERATURE temperature;
+  HUMIDITY humidity;
+};
+
+#endif
