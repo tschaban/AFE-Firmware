fea34dcc831cff0b0db6b9d4cab1c9edc930c34e
Czabanowski <czabanoa@emea.roche.com>
2018-01-14 23:31:06 +0100

    Thermostate and Humidistat structure

---
+++ lib/AFE-Data-Structure/AFE-REGULATOR-Structure.h
@@ -0,0 +1,24 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_REGULATOR_Structure_h
+#define _AFE_REGULATOR_Structure_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <ESP8266WiFi.h>
+
+struct REGULATOR {
+  boolean enabled;
+  float turnOn;
+  boolean turnOnAbove;
+  float turnOff;
+  boolean turnOffAbove;
+};
+
+#endif
