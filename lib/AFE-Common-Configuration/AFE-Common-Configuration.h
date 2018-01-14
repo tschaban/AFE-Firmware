f4a69e0cf693bb64dea9503fe2929baff782566d
Czabanowski <czabanoa@emea.roche.com>
2018-01-14 23:29:17 +0100

    Configuration for all firmwares

---
+++ lib/AFE-Common-Configuration/AFE-Common-Configuration.h
@@ -0,0 +1,18 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_Common_Configuration_h
+#define _AFE_Common_Configuration_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <ESP8266WiFi.h>
+
+#define SERVER_CMD_SAVE 1
+
+#endif
