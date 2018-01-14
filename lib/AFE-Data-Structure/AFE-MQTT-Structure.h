c6c0f53a93b616231f8da4544f07090e26689fb0
Czabanowski <czabanoa@emea.roche.com>
2018-01-14 23:30:13 +0100

    MQTT structure

---
+++ lib/AFE-Data-Structure/AFE-MQTT-Structure.h
@@ -0,0 +1,25 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_MQTT_Structure_h
+#define _AFE_MQTT_Structure_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <ESP8266WiFi.h>
+
+struct MQTT {
+  char host[32];
+  IPAddress ip;
+  uint16_t port;
+  char user[32];
+  char password[32];
+  char topic[32];
+};
+
+#endif
