2a76b46c3c30eb120e557ce9af67ab7319bce057
Czabanowski <czabanoa@emea.roche.com>
2018-01-14 23:31:27 +0100

    Switch Structure

---
+++ lib/AFE-Data-Structure/AFE-SWITCH-Structure.h
@@ -0,0 +1,28 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_SWITCH_Structure_h
+#define _AFE_SWITCH_Structure_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <ESP8266WiFi.h>
+
+#define SWITCH_TYPE_MONO 0 // Mono stable switch
+#define SWITCH_TYPE_BI 1   // Bistable switch
+
+#define SWITCH_MULTI 0
+
+struct SWITCH {
+  uint8_t gpio;
+  uint8_t type;
+  uint16_t sensitiveness;
+  uint8_t functionality;
+};
+
+#endif
