fae17f1716de6e568f2245874fb2c37c8aa45344
Czabanowski <czabanoa@emea.roche.com>
2018-01-14 23:30:02 +0100

    LED structure

---
+++ lib/AFE-Data-Structure/AFE-LED-Structure.h
@@ -0,0 +1,21 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_LED_Structure_h
+#define _AFE_LED_Structure_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <ESP8266WiFi.h>
+
+struct LED {
+  uint8_t gpio;
+  boolean changeToOppositeValue;
+};
+
+#endif
