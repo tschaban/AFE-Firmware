575256535365d2fdfe5fdae1c0b1932722e80541
Czabanowski <czabanoa@emea.roche.com>
2018-02-03 00:20:54 +0100

    added device structure

---
+++ T5/lib/AFE-Data-Structure/AFE-DEVICE-Structure.h
@@ -0,0 +1,30 @@
+/* AFE Firmware for smart home devices
+  LICENSE: https://github.com/tschaban/AFE-Firmware/blob/master/LICENSE
+  DOC: http://smart-house.adrian.czabanowski.com/afe-firmware-pl/ */
+
+#ifndef _AFE_DEVICE_Structure_h
+#define _AFE_DEVICE_Structure_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#define MODE_NORMAL 0
+#define MODE_CONFIGURATION 1
+#define MODE_ACCESS_POINT 2
+
+struct DEVICE {
+  char name[32];
+  boolean mqttAPI;
+  boolean httpAPI;
+  boolean domoticzAPI;
+  boolean isLED[2];
+  boolean isRelay[1];
+  boolean isSwitch[2];
+  boolean isContracton[2];
+  boolean isDHT;
+};
+
+#endif
