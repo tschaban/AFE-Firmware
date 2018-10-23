719db76c4a6a36861969470020c8f84751372a6b
Tschaban-A <github@adrian.czabanowski.com>
2018-10-23 23:53:15 +0200

    formating

--- T0/src/AFE-Events-Handler.ino
+++ T0/src/AFE-Events-Handler.ino
@@ -1,19 +1,24 @@
 void eventsListener() {
   /* Event handler: connection to wireless network has been established */
   if (Network.eventConnected()) {
-
-     /* Update relay status to Domoticz */
+#ifdef DEBUG
+    Serial << endl << "Events listener: triggered";
+#endif
+    /* Update relay status to Domoticz */
     if (Device.configuration.domoticzAPI) {
+#ifndef T0_SHELLY_1_CONFIG
       Led.on();
+#endif
       for (uint8_t i = 0; i < sizeof(Device.configuration.isRelay); i++) {
         if (Device.configuration.isRelay[i]) {
-            DomoticzPublishRelayState(i);
+          DomoticzPublishRelayState(i);
         } else {
           break;
         }
       }
+#ifndef T0_SHELLY_1_CONFIG
       Led.off();
+#endif
     }
   } /* End of Network.eventConnected() */
-
 }
