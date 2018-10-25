4864d8208e616ffb6622e14d75c58d204652d97a
Tschaban-A <github@adrian.czabanowski.com>
2018-10-25 20:45:57 +0200

    Tx- version

--- T0/lib/AFE-Configuration/AFE-Configuration.h
+++ T0/lib/AFE-Configuration/AFE-Configuration.h
@@ -14,7 +14,7 @@
 #if defined(T0_CONFIG)
 #define FIRMWARE_VERSION "1.2.4"
 #define FIRMWARE_TYPE 0
-#elif defined(T0_CONFIG) && defined(T0_SHELLY_1_CONFIG)
+#elif defined(T0_SHELLY_1_CONFIG)
 #define FIRMWARE_VERSION "S1.0.1"
 #define FIRMWARE_TYPE 0
 #elif defined(T4_CONFIG)
