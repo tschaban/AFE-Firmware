555875d73cc40f19820e26ec7c1bf07fa887e211
Tschaban-A <github@adrian.czabanowski.com>
2018-12-01 23:12:47 +0100

    Fixs

--- lib/AFE-Configuration/AFE-Configuration.h
+++ lib/AFE-Configuration/AFE-Configuration.h
@@ -21,16 +21,16 @@
 #define FIRMWARE_VERSION "1.3.1"
 #define FIRMWARE_TYPE 1
 #elif defined(T2_CONFIG)
-#define FIRMWARE_VERSION "1.3.1"
+#define FIRMWARE_VERSION "1.3.2"
 #define FIRMWARE_TYPE 2
 #elif defined(T3_CONFIG)
 #define FIRMWARE_TYPE 3
 #define FIRMWARE_VERSION "1.3.0"
 #elif defined(T4_CONFIG)
-#define FIRMWARE_VERSION "1.3.0"
+#define FIRMWARE_VERSION "1.3.1"
 #define FIRMWARE_TYPE 4
 #elif defined(T5_CONFIG)
-#define FIRMWARE_VERSION "1.3.1"
+#define FIRMWARE_VERSION "1.3.2"
 #define FIRMWARE_TYPE 5
 #elif defined(T6_CONFIG)
 #define FIRMWARE_VERSION "0.0.1"
