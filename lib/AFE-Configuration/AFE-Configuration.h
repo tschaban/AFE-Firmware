c3085db790941a9923af48cc753e73c79c5a3cbc
Tschaban-A <github@adrian.czabanowski.com>
2019-01-05 10:52:25 +0100

    changed version #

--- lib/AFE-Configuration/AFE-Configuration.h
+++ lib/AFE-Configuration/AFE-Configuration.h
@@ -33,7 +33,7 @@
 #define FIRMWARE_VERSION "1.3.2"
 #define FIRMWARE_TYPE 5
 #elif defined(T6_CONFIG)
-#define FIRMWARE_VERSION "0.0.4"
+#define FIRMWARE_VERSION "1.0.0"
 #define FIRMWARE_TYPE 6
 #endif
 
