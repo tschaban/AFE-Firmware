450ea9a2f3fdebf6bc591b1880d9cfabb475f412
Tschaban-A <github@adrian.czabanowski.com>
2022-05-13 13:09:19 +0200

    T5-3.3.0

--- core/include/config/builds/AFE-T5-Build.h
+++ core/include/config/builds/AFE-T5-Build.h
@@ -1,6 +1,6 @@
 
 #ifndef _AFE_Configuration_Build_h
 #define _AFE_Configuration_Build_h
-#define AFE_VERSION_BUILD_NUMBER 336
+#define AFE_VERSION_BUILD_NUMBER 1
 #define AFE_VERSION_BUILD_DATE "2022-05-13 12:17:24.926076"
 #endif  
--- core/include/config/firmwares/AFE-T5.h
+++ core/include/config/firmwares/AFE-T5.h
@@ -1,7 +1,7 @@
 #ifndef _AFE_Configuration_T_h
 #define _AFE_Configuration_T_h
 
-#define AFE_FIRMWARE_VERSION "3.1.0"
+#define AFE_FIRMWARE_VERSION "3.3.0"
 #define AFE_FIRMWARE_TYPE 5
 
 /* Hardware */
