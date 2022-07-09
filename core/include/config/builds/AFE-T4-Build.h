e72a5eaa204b31dfecdee86874f3e339945320db
Tschaban-A <github@adrian.czabanowski.com>
2022-07-09 20:39:42 +0200

    T4-3.4.1

--- core/include/config/builds/AFE-T4-Build.h
+++ core/include/config/builds/AFE-T4-Build.h
@@ -1,6 +1,6 @@
 
 #ifndef _AFE_Configuration_Build_h
 #define _AFE_Configuration_Build_h
-#define AFE_VERSION_BUILD_NUMBER 30
-#define AFE_VERSION_BUILD_DATE "2022-07-06 21:19:05.999170"
+#define AFE_VERSION_BUILD_NUMBER 32
+#define AFE_VERSION_BUILD_DATE "2022-07-09 20:37:27.228343"
 #endif  
--- tools/main.php
+++ tools/main.php
@@ -3,9 +3,9 @@
 
 /* Set this before run */
 
-$type = "6";
+$type = "4";
 $version = "3.4.1";
-$language = "en";
+$language = "pl";
 $development = false;
 
 
