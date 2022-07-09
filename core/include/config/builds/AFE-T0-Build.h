97160f41be75ac3291c0426f620dd3ba2ab1ec5d
Tschaban-A <github@adrian.czabanowski.com>
2022-07-09 20:18:52 +0200

    T4-3.4.1

--- core/include/config/builds/AFE-T0-Build.h
+++ core/include/config/builds/AFE-T0-Build.h
@@ -1,6 +1,6 @@
 
 #ifndef _AFE_Configuration_Build_h
 #define _AFE_Configuration_Build_h
-#define AFE_VERSION_BUILD_NUMBER 810
-#define AFE_VERSION_BUILD_DATE "2022-07-08 22:06:10.897368"
+#define AFE_VERSION_BUILD_NUMBER 812
+#define AFE_VERSION_BUILD_DATE "2022-06-10 10:21:38.091256"
 #endif  
