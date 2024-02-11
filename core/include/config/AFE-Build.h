296d8d5015243291366c1540cfd1330631f59df4
Tschaban-A <github@adrian.czabanowski.com>
2024-02-11 22:19:46 +0100

    Tx-3.8.0.B9
    added new params to platformio

--- core/include/config/AFE-Build.h
+++ core/include/config/AFE-Build.h
@@ -1,6 +1,6 @@
 
 #ifndef _AFE_Configuration_Build_h
 #define _AFE_Configuration_Build_h
-#define AFE_VERSION_BUILD_NUMBER 2667
-#define AFE_VERSION_BUILD_DATE "2024-02-11 22:08:21.308323"
+#define AFE_VERSION_BUILD_NUMBER 2672
+#define AFE_VERSION_BUILD_DATE "2024-02-11 22:19:09.084338"
 #endif  
