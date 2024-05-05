9d17705896bd3b545891fa2fdfb9a2f3c1f8e60e
Tschaban-A <github@adrian.czabanowski.com>
2024-05-05 15:15:41 +0200

    Tx-3.8.0.B12

--- core/include/config/AFE-Build.h
+++ core/include/config/AFE-Build.h
@@ -1,6 +1,6 @@
 
 #ifndef _AFE_Configuration_Build_h
 #define _AFE_Configuration_Build_h
-#define AFE_VERSION_BUILD_NUMBER 3175
-#define AFE_VERSION_BUILD_DATE "2024-04-20 10:37:16.493149"
+#define AFE_VERSION_BUILD_NUMBER 3205
+#define AFE_VERSION_BUILD_DATE "2024-05-05 15:07:57.914049"
 #endif  
